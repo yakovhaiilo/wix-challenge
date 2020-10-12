2fdf533119099a460dee8740f70014a9f3040c05
yakovhaiilo <yakovhailo123@gmail.com>
2020-10-12 12:55:26 +0300

    add a filter panel & refactoring

--- client/src/pages/BooksSearch/BooksSearch.js
+++ client/src/pages/BooksSearch/BooksSearch.js
@@ -1,41 +1,36 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useContext } from "react";
 import axios from "axios";
 
 import "./BookSearch.css";
-import Book from "../../components/Book/Book";
+import { BooksContext } from "../../context/BooksContext";
+
+// components
+import TextFields from "../../components/TextFields/TextFields";
 import Spiner from "../../components/Spiner/Spiner";
 import PaginationOutlined from "../../components/Pagination/PaginationOutlined";
-import RangeSlider from "../../components/Slider/RangeSlider";
+import FilterBar from "../../components/FilterBar/FilterBar";
+import BooksList from "../../components/BooksList/BooksList";
 
-//MuI
-import { Container, Grid } from "@material-ui/core";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-
-import { ToastContainer } from "react-toastify";
-
-// Building upon the first assignment, they should add a filter panel which lets filter
-// the book list by author and by range of publication years.
-// The author filter should be applied as you type
-// (only when there are more than 2 chars in the filter) and the year range should be applied using a button.
-
-export const BooksSearch = ({ books, updateBooks, clearBooks }) => {
+export const BooksSearch = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [filterAuthor, setFilterAuthor] = useState("");
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
-  const [totalFound, setTotalFound] = useState(null);
-  const [rangeYears, setRangeYears] = useState([500, 1500]);
+  const [yearsRange, setYearsRange] = useState([500, 1500]);
   const [filterIsValid, setFilterIsValid] = useState(false);
+  const {
+    books,
+    updateBooks,
+    clearBooks,
+    totalFound,
+    setTotalFound,
+  } = useContext(BooksContext);
   const mounted = useRef(true);
   const limit = 20;
   const pages = totalFound ? Math.ceil(totalFound / limit) : null;
 
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
   const onPage = (p = 1) => {
+    console.log("onpage", p);
     if (books[p]) {
       return setPage(p);
     }
@@ -69,90 +64,57 @@ export const BooksSearch = ({ books, updateBooks, clearBooks }) => {
     }
   };
 
-  useEffect(() => {
-    return () => {
-      mounted.current = false;
-    };
-  }, []);
-
   const filterAuthorName = (book) => {
     let name = (book.author_name && book.author_name[0]) || "";
     return name.toUpperCase().includes(filterAuthor.toUpperCase());
   };
   const filterYearsRange = (book) => {
     return (
-      book.publish_year > rangeYears[0] && book.publish_year < rangeYears[1]
+      book.publish_year >= yearsRange[0] && book.publish_year <= yearsRange[1]
     );
   };
 
   const setfilters = (book) => {
-    if (filterAuthor.length > 2 && !filterIsValid) {
-      return filterAuthorName(book);
-    } else if (!(filterAuthor.length > 2) && filterIsValid) {
-      return filterYearsRange(book);
-    } else if (filterAuthor.length > 2 && filterIsValid) {
-      return filterYearsRange(book) && filterAuthorName(book);
-    } else {
-      return true;
-    }
+    let authorFilter = filterIsValid ? filterYearsRange(book) : true;
+    let yeasrsFilter = filterAuthor.length > 2 ? filterAuthorName(book) : true;
+    return authorFilter && yeasrsFilter;
   };
+
+  useEffect(() => {
+    return () => {
+      mounted.current = false;
+    };
+  }, []);
+
+  // console.log("b", books);
+  // console.log("b", page, pages);
   return (
     <div className="booksSearch">
-      <ToastContainer />
-      <div className="booksSearch__form">
-        <TextField
-          id="standard-basic"
-          label="filter autor name"
-          value={filterAuthor}
-          onChange={(e) => setFilterAuthor(e.target.value)}
-        />
-
-        <TextField
-          id="standard-basic"
-          label="search for a book"
+      <div className="booksSearch__TextFields">
+        <TextFields
+          onSubmit={onSearch}
+          setValue={setSearchTerm}
           value={searchTerm}
-          onChange={handleChange}
+          btnText={"Search"}
+          label={"search for a book"}
         />
-        <Button
-          variant="contained"
-          onClick={() => onSearch()}
-          color="secondary"
-        >
-          search
-        </Button>
-        <RangeSlider setRangeYears={setRangeYears} rangeYears={rangeYears} />
-        <div>
-          <Button
-            variant="contained"
-            onClick={() => setFilterIsValid(!filterIsValid)}
-            color="secondary"
-          >
-            {filterIsValid ? "Resat filter" : "Filter Years"}
-          </Button>
-        </div>
+      </div>
+      <div className="booksSearch__filterBar">
+        <FilterBar
+          setYearsRange={setYearsRange}
+          yearsRange={yearsRange}
+          filterIsValid={filterIsValid}
+          setFilterIsValid={setFilterIsValid}
+          filterAuthor={filterAuthor}
+          setFilterAuthor={setFilterAuthor}
+        />
+      </div>
+      <div className="booksSearch__Pagination">
+        <PaginationOutlined page={page} pages={pages} renderPage={onPage} />
       </div>
 
-      {pages && (
-        <PaginationOutlined page={page} pages={pages} renderPage={onPage} />
-      )}
-
-      <Container>
-        <Grid
-          container
-          direction="row-reverse"
-          justify="center"
-          alignItems="center"
-        >
-          {!isLoading ? (
-            books[page] &&
-            books[page]
-              .filter(setfilters)
-              .map((book, i) => <Book book={book} key={i} />)
-          ) : (
-            <Spiner />
-          )}
-        </Grid>
-      </Container>
+      {!isLoading && <BooksList books={books[page]} boolean={setfilters} />}
+      {isLoading && <Spiner />}
     </div>
   );
 };
